68675c720ee26574949205b0118612c27a19746c
Palash1798 <kpalash1998@gmail.com>
2024-10-26 11:51:31 +0530

    Failed test for adding new line

--- src/test/java/org/example/stringcalculatortdd/StringCalculatorTddApplicationTests.java
+++ src/test/java/org/example/stringcalculatortdd/StringCalculatorTddApplicationTests.java
@@ -31,4 +31,9 @@ class StringCalculatorTddApplicationTests {
     public void testAnyAmountOfNumbers(){
         assertEquals(6, StringCalculator.add("1,2,3"));
     }
+
+    @Test
+    public void testNewLine(){
+        assertEquals(6, StringCalculator.add("1\n2,3"));
+    }
 }
