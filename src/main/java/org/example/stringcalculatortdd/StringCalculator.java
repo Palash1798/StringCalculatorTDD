91cea6156fb2c27913447902e83ac2a0bb5d8977
Palash1798 <kpalash1998@gmail.com>
2024-10-26 11:32:42 +0530

    Working code for empty String test

---
+++ src/main/java/org/example/stringcalculatortdd/StringCalculator.java
@@ -0,0 +1,11 @@
+package org.example.stringcalculatortdd;
+
+public class StringCalculator {
+
+    public static int add(String input) {
+        if(input.isEmpty()) {
+            return 0;
+        }
+        return -1;   // Taking -1 just to pass the empty String test case.
+    }
+}
