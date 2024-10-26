e5c261f84f2f196446c46f5adb82e26f140d519f
Palash1798 <kpalash1998@gmail.com>
2024-10-26 11:41:38 +0530

    Working code for adding two numbers

--- src/main/java/org/example/stringcalculatortdd/StringCalculator.java
+++ src/main/java/org/example/stringcalculatortdd/StringCalculator.java
@@ -6,6 +6,18 @@ public class StringCalculator {
         if(input.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(input) ;
+
+        //code for adding two numbers
+        String[] numbers = input.split(",");
+        int sum = 0;
+        if(numbers.length > 1) {
+            sum = toInt(numbers[0]) + toInt(numbers[1]);
+        }
+
+        return sum;
+    }
+
+    public static int toInt(String number) {
+        return Integer.parseInt(number);
     }
 }
